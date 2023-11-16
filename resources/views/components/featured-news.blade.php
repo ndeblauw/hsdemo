804fd60f5f791c46d6bce199ccb61bcd1ce374fa
Nico Deblauwe <nico@bluepundit.eu>
2023-11-16 16:25:30 +0100

    mix

--- resources/views/components/featured-news.blade.php
+++ resources/views/components/featured-news.blade.php
@@ -1,6 +1,9 @@
 <div class="bg-gray-300 pt-12">
+
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h2 class="font-bold">Featured for you today</h2>
+        <div class="flex justify-center mx-4">
+            <h2 class="text-lg border-t border-b mb-4 border-gray-500 ">Featured for you today</h2>
+        </div>
         <div class="grid grid-cols-4 gap-4">
             @foreach($featured_news as $post)
                 <div class="bg-white p-2">
