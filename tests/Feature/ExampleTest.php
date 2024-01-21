e33366abe5022de9b2918a2513f337004b5d53fc
Nico Deblauwe <nico@bluepundit.eu>
2024-01-21 14:57:16 +0100

    Remove redundant tests

--- tests/Feature/ExampleTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-it('returns a successful response', function () {
-    $response = $this->withSession(['weather_show_plugin_city' => 'brussels'])->get('/');
-
-    $response->assertStatus(200);
-});
